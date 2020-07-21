241a96447f0bd088934b5dd4066de517eea02690
Joshua Zhou <joshua.j.zhou@gmail.com>
2020-07-20 23:57:47 -0700

    Emacs config updates

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,11 @@
 ;; [2020-07-20] Disable menu bar
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (menu-bar-mode -1)
 
 ;; [2020-07-20] Change colour of the status bar / modeline
@@ -13,5 +20,13 @@
 ;; (set-face-foreground 'linum "white")
 ;; (set-face-background 'linum "black")
 
+;; [2020-07-20] Add column position to modeline
+(setq column-number-mode t)
 
+;; [2020-07-20] Package setup
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 
+;; [2020-07-20] Add vi tildes
+(load "~/config/emacs/vim-empty-lines-mode.el")
+(vim-empty-lines-mode)
