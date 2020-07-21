8ac1d6b1d184c666e52fddccf28fe3682e75857f
Joshua Zhou <joshua.j.zhou@gmail.com>
2020-07-21 08:24:50 -0700

    And I am stop and go in your eyes

--- emacs/init.el
+++ emacs/init.el
@@ -26,7 +26,33 @@
 ;; [2020-07-20] Package setup
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
 
 ;; [2020-07-20] Add vi tildes
 (load "~/config/emacs/vim-empty-lines-mode.el")
-(vim-empty-lines-mode)
+(global-vim-empty-lines-mode)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (haskell-mode use-package dashboard))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; [2020-07-20] Dashboard startup splash screen config
+(use-package dashboard
+	     :ensure t
+	     :diminish dashboard-mode
+	     :config
+	     (setq dashboard-set-footer nil)
+	     (setq dashboard-banner-logo-title "your custom t")
+	     (setq dashboard-startup-banner 2)
+	     (setq dashboard-items '((recents  . 10)
+				     (bookmarks . 10)))
+	         (dashboard-setup-startup-hook))
